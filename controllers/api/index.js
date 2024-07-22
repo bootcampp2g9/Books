4b7a1036f5f90668b716e0d862fe3187772f6968
gerikoci31 <165694929+gerikoci31@users.noreply.github.com>
2024-07-22 19:50:28 -0400

    api folder routes

---
+++ controllers/api/index.js
@@ -0,0 +1,8 @@
+const router = require('express').Router();// require express and create router
+const userRoutes = require('./userRoutes');//import route handler
+const projectRoutes = require('./projectRoutes');//import route handler
+const apiRoutes = require('./api');
+router.use('/users', userRoutes);//requests starting with users will be routed to userRoutes
+router.use('/projects', projectRoutes);//requests starting with projects will be router to projectRoutes
+router.use('/api',apiRoutes);//reuqests made to /api route will be routed to index.js 
+module.exports = router;
