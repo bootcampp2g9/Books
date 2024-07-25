3425b6794ee70e5e95cfaa7566dfdd726f6e230d
Brian Moura <brian.prorealestate@gmail.com>
2024-07-24 20:00:00 -0400

    removed withauth middleware, removed and cleaned more code

--- controllers/homeRoutes.js
+++ controllers/homeRoutes.js
@@ -1,6 +1,6 @@
 const router = require('express').Router();
 const { Book, User } = require('../models'); // Import the book and user models from the models directory
-const withAuth = require('../utils/auth'); // Import the withAuth middleware to protect routes
+// const withAuth = require('../utils/auth'); // Import the withAuth middleware to protect routes
 
 // Route to handle GET requests for the homepage
 router.get('/', async (req, res) => {
@@ -56,24 +56,24 @@ router.get('/Book/:id', async (req, res) => {
 
 // Use withAuth middleware to prevent access to route
 // Route to handle GET requests for the profile page, using withAuth middleware, include the users books
-router.get('/profile', withAuth, async (req, res) => {
-  try {
-    // Find the logged in user based on the session ID
-    const userData = await User.findByPk(req.session.user_id, {
-      attributes: { exclude: ['password'] },
-      include: [{ model: Project }],
-    });
+// // router.get('/profile', withAuth, async (req, res) => {
+// //   try {
+// //     // Find the logged in user based on the session ID
+// //     const userData = await User.findByPk(req.session.user_id, {
+// //       attributes: { exclude: ['password'] },
+// //       include: [{ model: Project }],
+// //     });
 
-    const user = userData.get({ plain: true });
+//     const user = userData.get({ plain: true });
 
-    res.render('profile', {
-      ...user,
-      logged_in: true
-    });
-  } catch (err) {
-    res.status(500).json(err);
-  }
-});
+//     res.render('profile', {
+//       ...user,
+//       logged_in: true
+//     });
+//   } catch (err) {
+//     res.status(500).json(err);
+//   }
+// });
 
 // Route to handle GET requests for the login page
 router.get('/login', (req, res) => {
