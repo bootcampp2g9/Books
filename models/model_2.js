966112614284c950451df0022d245998982f3129
AeroRider <dave.butterworth.ee@gmail.com>
2024-07-25 16:43:01 -0500

    to fix the book listing

---
+++ models/model_2.js
@@ -0,0 +1,30 @@
+
+const express = require('express')
+
+const booksList = [{
+        bookID: "ID 1",
+        bookISBN: "ISBN 1",
+        bookTitle: "Title 1",
+        bookAuthor: "Number_One_Writer",
+        bookPages: 111,
+        bookEdition: 1,
+        bookPublisher_ID: "Publisher 1 ID",
+        bookPubYear: "Publication Year 1",
+        bookCategory: "Science Fiction 1",
+        bookDescription: "Story of man who lived forever. 1",
+        bookIDStatus: "Checked In/Out 1"
+    },
+    {
+        bookID: "ID 2",
+        bookISBN: "ISBN 2",
+        bookTitle: "Title 2",
+        bookAuthor: "Number_Two_Writer",
+        bookPages: 222,
+        bookEdition: 2,
+        bookPublisher_ID: "Publisher 2 ID",
+        bookPubYear: "Publication Year 2",
+        bookCategory: "Biography 2",
+        bookDescription: "Biography of  2",
+        bookIDStatus: "Checked In/Out 2"
+    }
+]
