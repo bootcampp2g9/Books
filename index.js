700c4f817563b5b6d7f58e0ee57bc38eb8b34bd9
gerikoci31 <165694929+gerikoci31@users.noreply.github.com>
2024-07-25 21:29:10 -0400

    feature/api is now able to look up books by their title

--- index.js
+++ index.js
@@ -1,28 +1,41 @@
 const express = require('express');
 const axios = require('axios');
 
+const exphbs = require('express-handlebars');
+const helpers = require('./utils/helpers');
+
 const app = express();
 const PORT = process.env.PORT || 3000;
 
-// Function to fetch book details by ISBN
-async function fetchBookDetails(isbn) {
+// Set up Handlebars.js engine with custom helpers
+const hbs = exphbs.create({ helpers });
+
+// Inform Express.js on which template engine to use
+app.set('view engine', 'handlebars');
+app.engine('handlebars', hbs.engine);
+
+// Function to fetch book details by title
+async function fetchBookDetails(title) {
   try {
-    const response = await axios.get(`https://openlibrary.org/api/books?bibkeys=ISBN:${isbn}&format=json&jscmd=data`);
-    console.log(response.data);
-    return response.data;
+    const url = `https://openlibrary.org/search.json?title=${encodeURIComponent(title)}`;
+    const response = await axios.get(url);
+    return response.data.docs; // Return the array of books
   } catch (error) {
     console.error('Error fetching book details:', error);
-    return null;
+    throw new Error('Error fetching book details');
   }
 }
 
+
 // API endpoint to fetch book details by ISBN
-app.get('/api/book/:isbn', async (req, res) => {
-  const isbn = req.params.isbn;
+app.get('/api/book/:title', async (req, res) => {
+  const title = req.params.title;
   try {
-    const bookDetails = await fetchBookDetails(isbn);
-    if (bookDetails && `ISBN:${isbn}` in bookDetails) {
-      res.json(bookDetails[`ISBN:${isbn}`]);
+    const bookDetails = await fetchBookDetails(title);
+
+    if (bookDetails && bookDetails.length > 0) {
+      // Return the first book in the results
+      res.json(bookDetails[0]);
     } else {
       res.status(404).json({ message: 'Book not found.' });
     }
@@ -31,6 +44,13 @@ app.get('/api/book/:isbn', async (req, res) => {
     res.status(500).json({ message: 'Internal Server Error' });
   }
 });
+
+
+app.get('/', async(req, res) => {
+  res.render('login');
+});
+// const homeRouter = require('./controllers/homeRoutes');
+// app.use('/', homeRouter)
 
 // Start the server
 app.listen(PORT, () => {
